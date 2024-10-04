df0eceac0d01d045708f5e596481a0ca29b54bf0
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-10-04 14:15:13 +0530

    Improved button style

--- client/src/components/Header/CustomButtons.jsx
+++ client/src/components/Header/CustomButtons.jsx
@@ -1,76 +1,85 @@
-import { Badge, Box, Button, styled, Typography } from "@mui/material"
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import Login from "../Login/Login"
-import { useContext, useState } from "react"
-import { DataContext } from "../../context/DataProvider"
-import Profile from "./Profile"
-import { useNavigate } from "react-router-dom"
-import { useSelector } from "react-redux"
+import { Badge, Box, Button, styled, Typography } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Login from "../Login/Login";
+import { useContext, useState } from "react";
+import { DataContext } from "../../context/DataProvider";
+import Profile from "./Profile";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const BoxStyle=styled(Box)(({theme}) => ({
-    display : "flex",
-    margin : "0 3% 0 auto",
-    '& > *' : {
-    marginRight : "40px !important",
-    fontSize : "14px",
-    alignItems : "center"
-    },
-    [theme.breakpoints.down('md')]:{
-        display : "block"
-    }
+const BoxStyle = styled(Box)(({ theme }) => ({
+  display: "flex",
+  margin: "0 3% 0 auto",
+  "& > *": {
+    marginRight: "40px !important",
+    fontSize: "14px",
+    alignItems: "center",
+  },
+  [theme.breakpoints.down("md")]: {
+    display: "block",
+  },
 }));
 
-const BoxShoppingCart=styled(Box)(({theme}) => ({
-    display : "flex",
-    cursor : "pointer",
-    [theme.breakpoints.down('md')]:{
-        display : "block"
-    }
+const BoxShoppingCart = styled(Box)(({ theme }) => ({
+  display: "flex",
+  cursor: "pointer",
+  [theme.breakpoints.down("md")]: {
+    display: "block",
+  },
 }));
 
- const ButtonStyle=styled(Button)`
- color : #2874f0;
- background : #FFFFFF;
- text-transform : none;
- padding : 5px 40px;
- border-radius : 2px;
- box-shadow : none;
- font-weight : 600;
- height : 32px;
-`
+const ButtonStyle = styled(Button)`
+  color: #2874f0;
+  background: #ffffff;
+  text-transform: none;
+  padding: 5px 40px;
+  border-radius: 2px;
+  box-shadow: none;
+  font-weight: 600;
+  height: 32px;
+  &:hover {
+    background: #ffffff;
+    box-shadow: none;
+  }
+`;
 
 const CustomButtons = () => {
-    const [ open, setOpen ] = useState(false);
-    const navigate = useNavigate();
-    const { cartItems } = useSelector(state => state.cart);
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const { cartItems } = useSelector((state) => state.cart);
 
-    const handleLogin = () => {
-        setOpen(true);
-    }
+  const handleLogin = () => {
+    setOpen(true);
+  };
 
-    const { account, setAccount } = useContext(DataContext);
+  const { account, setAccount } = useContext(DataContext);
 
-    const handleNavigation = () => {
-        navigate('/cart');
-    }
+  const handleNavigation = () => {
+    navigate("/cart");
+  };
 
-    return (
-        <BoxStyle>
-            {
-                account ? <Profile account={account} setAccount={setAccount}/> :
-                <ButtonStyle variant="contained" onClick={handleLogin}>Login</ButtonStyle>
-            }
-            <Typography style={{marginTop: 3, width:135}}>Become a Seller</Typography>
-            <Typography style={{marginTop: 3}}>More</Typography>
-            <BoxShoppingCart onClick={handleNavigation}>
-                <Badge badgeContent={cartItems?.length} color="secondary">
-                <ShoppingCartIcon/>
-                </Badge>
-                <Typography style={{marginLeft: "10px"}}>Cart</Typography>
-            </BoxShoppingCart>
-            <Login open={open} setOpen={setOpen}/>
-        </BoxStyle>
-    )
-}
+  return (
+    <BoxStyle>
+      {account ? (
+        <Profile account={account} setAccount={setAccount} />
+      ) : (
+        <ButtonStyle variant="contained" onClick={handleLogin}>
+          Login
+        </ButtonStyle>
+      )}
+      <Typography style={{ marginTop: 3, width: 135, cursor: 'pointer' }}>
+        Become a Seller
+      </Typography>
+      <Typography style={{ marginTop: 3, cursor: 'pointer' }}>More</Typography>
+      <BoxShoppingCart onClick={handleNavigation}>
+        <Badge badgeContent={cartItems?.length} color="secondary">
+          <ShoppingCartIcon />
+        </Badge>
+        <Typography style={{ marginLeft: "10px" }}>Cart</Typography>
+      </BoxShoppingCart>
+      <Login open={open} setOpen={setOpen} />
+    </BoxStyle>
+  );
+};
 
 export default CustomButtons;
