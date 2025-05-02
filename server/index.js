353797569b636244fa7d99329024f1fb6a6b1cef
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-05-02 13:31:21 +0530

    corrected cookies

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.set("trust proxy", 1);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
-app.use(cors({ origin: true, withCredentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 import user from "./routes/user.route.js";
 import product from "./routes/product.route.js";
